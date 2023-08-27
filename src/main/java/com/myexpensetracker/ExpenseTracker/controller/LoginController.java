dd54c26dac198842102b79cb9ffe15284e638286
mathh1337 <112662623+matipt2@users.noreply.github.com>
2023-08-28 00:01:41 +0200

    loggin in logic

--- src/main/java/com/myexpensetracker/ExpenseTracker/controller/LoginController.java
+++ src/main/java/com/myexpensetracker/ExpenseTracker/controller/LoginController.java
@@ -1,13 +1,33 @@
 package com.myexpensetracker.ExpenseTracker.controller;
 
+import com.myexpensetracker.ExpenseTracker.model.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
 public class LoginController {
+
     @GetMapping("/login")
     public String getLogin(){
         return "login";
     }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute(name ="loginForm")LoginForm loginForm, Model model){
+        String username = loginForm.getUsername();
+        String password = loginForm.getPassword();
+        if("admin".equals(username) && "admin".equals(password)){
+            return "redirect:/expense";
+        }
+        model.addAttribute("errorMessage", "FAILED TO LOGIN");
+        return "login";
+    }
 }
+
+
+
+
