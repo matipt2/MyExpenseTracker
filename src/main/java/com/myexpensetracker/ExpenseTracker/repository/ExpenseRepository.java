b6855253c90bef3af6a9c3d1c951fcd1452c597d
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-02-14 16:47:10 +0100

    mysql database

--- src/main/java/com/myexpensetracker/ExpenseTracker/repository/ExpenseRepository.java
+++ src/main/java/com/myexpensetracker/ExpenseTracker/repository/ExpenseRepository.java
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-
-}
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
