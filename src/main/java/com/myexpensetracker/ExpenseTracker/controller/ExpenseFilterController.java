140513f27fac5d733e8e5eb4b9aaf74b3ca46ee4
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-02-19 00:14:17 +0100

    add expenses filter

--- src/main/java/com/myexpensetracker/ExpenseTracker/controller/ExpenseFilterController.java
+++ src/main/java/com/myexpensetracker/ExpenseTracker/controller/ExpenseFilterController.java
@@ -1,4 +1,34 @@
 package com.myexpensetracker.ExpenseTracker.controller;
+import com.myexpensetracker.ExpenseTracker.model.Expense;
+import com.myexpensetracker.ExpenseTracker.repository.ExpenseRepository;
+import com.myexpensetracker.ExpenseTracker.service.ExpenseFilterService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Controller
 public class ExpenseFilterController {
+    private ExpenseRepository expenseRepository;
+
+
+    public ExpenseFilterController(ExpenseRepository expenseRepository){
+        this.expenseRepository = expenseRepository;
+    }
+
+    @GetMapping("/filterByMinimum")
+    public String filterExpenseByMin(Model model){
+        ExpenseFilterService expenseFilterService = new ExpenseFilterService(expenseRepository);
+        List<Expense> filteredExpenses = expenseFilterService.filterByMinimum();
+        model.addAttribute("expenses", filteredExpenses);
+        return "expense";
+    }
+    @GetMapping("/filterByMaximum")
+    public String filterExpenseByMax(Model model){
+        ExpenseFilterService expenseFilterService = new ExpenseFilterService(expenseRepository);
+        List<Expense> filteredExpenses = expenseFilterService.filterByMaximum();
+        model.addAttribute("expenses", filteredExpenses);
+        return "expense";
+    }
 }
--- src/main/java/com/myexpensetracker/ExpenseTracker/service/ExpenseFilterService.java
+++ src/main/java/com/myexpensetracker/ExpenseTracker/service/ExpenseFilterService.java
@@ -1,4 +1,31 @@
 package com.myexpensetracker.ExpenseTracker.service;
+import com.myexpensetracker.ExpenseTracker.model.Expense;
+import com.myexpensetracker.ExpenseTracker.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ExpenseFilterService {
+    private final ExpenseRepository expenseRepository;
+
+    @Autowired
+    public ExpenseFilterService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+    public List<Expense> filterByMinimum() {
+        List<Expense> expenseList = expenseRepository.findAll().stream()
+                .sorted(Comparator.comparing(Expense::getPrice))
+                .collect(Collectors.toList());
+        return expenseList;
+
+    }
+    public List<Expense> filterByMaximum() {
+        List<Expense> expenseList = expenseRepository.findAll().stream()
+                .sorted(Comparator.comparing(Expense::getPrice).reversed())
+                .collect(Collectors.toList());
+        return expenseList;
+    }
 }
