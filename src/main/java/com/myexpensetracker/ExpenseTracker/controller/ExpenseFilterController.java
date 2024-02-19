41e3f8f39b19120836a14ae747a0eb0909625688
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-02-19 19:54:52 +0100

    fix filter problem

--- src/main/java/com/myexpensetracker/ExpenseTracker/controller/ExpenseFilterController.java
+++ src/main/java/com/myexpensetracker/ExpenseTracker/controller/ExpenseFilterController.java
@@ -5,13 +5,12 @@ import com.myexpensetracker.ExpenseTracker.service.ExpenseFilterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
 public class ExpenseFilterController {
-    private ExpenseRepository expenseRepository;
-
+    private final ExpenseRepository expenseRepository;
 
     public ExpenseFilterController(ExpenseRepository expenseRepository){
         this.expenseRepository = expenseRepository;
@@ -21,14 +20,19 @@ public class ExpenseFilterController {
     public String filterExpenseByMin(Model model){
         ExpenseFilterService expenseFilterService = new ExpenseFilterService(expenseRepository);
         List<Expense> filteredExpenses = expenseFilterService.filterByMinimum();
+        filteredExpenses.sort(Comparator.comparing(Expense::getPrice));
         model.addAttribute("expenses", filteredExpenses);
+        model.addAttribute("newExpense", new Expense());
         return "expense";
     }
+
     @GetMapping("/filterByMaximum")
     public String filterExpenseByMax(Model model){
         ExpenseFilterService expenseFilterService = new ExpenseFilterService(expenseRepository);
         List<Expense> filteredExpenses = expenseFilterService.filterByMaximum();
+        filteredExpenses.sort(Comparator.comparing(Expense::getPrice).reversed());
         model.addAttribute("expenses", filteredExpenses);
+        model.addAttribute("newExpense", new Expense());
         return "expense";
     }
 }
